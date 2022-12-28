74cf0a53f930c7ff49570a4eb195407277ffb87d
talhamirza1 <talhamehmood1073@gmail.com>
2022-12-28 22:08:38 +0500

    Jokes App Practice from Blog

---
+++ app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,33 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData, useParams } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+  if (!joke) {
+    throw new Error("Joke not found");
+  }
+  return json({ joke });
+};
+
+export function ErrorBoundary() {
+  const { jokeId } = useParams();
+  return (
+    <div className="error-container">{`There was an error loading joke by the id ${jokeId}. Sorry.`}</div>
+  );
+}
+
+export default function JokeRoute() {
+  const data = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <p>Here's your hilarious joke:</p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
+    </div>
+  );
+}
